a69bde9b838c6b13e2d0ca35ac99e69681fa4a1b
Lucas Guillerot lguiller <lguiller@student.42.fr>
2026-03-23 17:05:27 +0100

    add fist requests to get headers

--- update_balance_history/update_balance_history.py
+++ update_balance_history/update_balance_history.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from os.path import exists
 from datetime import datetime, timedelta
 
+URL_BASE = "https://www.e-chargement.com/default.Asp"
 URL = "https://www.e-chargement.com/identif_badge.Asp"
 BADGE_DIV = getenv("BADGE_DIV")
 BADGE_NUMBER = getenv("BADGE_NUMBER")
@@ -18,13 +19,25 @@ def estimate_meal_price(previous_balance, current_balance, previous_incoming_cre
 def main():
     session = requests.Session()
 
+    headers_base = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0',
+        'Accept-Language': 'en-US,en;q=0.9',
+    }
+    session.get(URL_BASE, headers=headers_base)
+
+    headers_post = headers_base.copy()
+    headers_post.update({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://www.e-chargement.com',
+        'Referer': URL_BASE,
+    })
     data = {
         "badge_div": BADGE_DIV,
         "badge_number": BADGE_NUMBER,
         "badge_nom": BADGE_NAME,
     }
 
-    response = session.post(URL, data=data, allow_redirects=True)
+    response = session.post(URL, data=data, allow_redirects=True, headers=headers_post)
     response.encoding = "utf-8"
 
     parsed_html = BeautifulSoup(response.text, "html.parser")
